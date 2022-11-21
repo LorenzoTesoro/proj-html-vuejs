11bb9f8d5ab0fe7858db976a36ca13753be09b17
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-21 23:17:52 +0100

    add book_lesson section

--- src/components/layout/AppMain.vue
+++ src/components/layout/AppMain.vue
@@ -1,37 +1,64 @@
 <script>
+import HeroSection from "../main_sections/HeroSection.vue";
+
 export default {
   name: "AppMain",
+  components: {
+    HeroSection,
+  },
 };
 </script>
 
 <template>
   <main>
-    <section class="jumbotron">
-      <div class="container d-flex position-relative">
-        <h1>Drive with Avada</h1>
-        <p>We offer the finest driving tuition money can buy</p>
-        <div class="square cost rounded">
-          <div class="price">$ 39</div>
-          <div class="on_sale">On Sale</div>
-        </div>
-        <div class="square demos rounded">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-calendar3-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2H0zm0 1v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3H0z"
-            />
-          </svg>
-          <div class="on_sale">Demos</div>
+    <HeroSection />
+    <!-- MANCA ICONA NELL'INPUT NAME -->
+    <section class="book_lesson pb-3">
+      <div class="container">
+        <div class="row">
+          <div class="col title">
+            <h2>Get Behind The Wheel</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Voluptatum hic nostrum error sunt officia doloribus provident vel
+              harum dolore qui. Sunt natus corporis ab laborum quibusdam omnis
+              expedita at vitae.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+              nobis delectus recusandae ad incidunt odit, excepturi beatae
+              tenetur earum eligendi odio, ea omnis pariatur tempore veniam
+              officiis aliquid iusto? Perferendis? Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Asperiores labore dicta vitae
+              excepturi, recusandae odio est corrupti dolore officiis delectus
+              eaque repellendus, neque placeat inventore quo perferendis, odit
+              quibusdam aspernatur.
+            </p>
+          </div>
+          <!--/.title -->
+          <div class="col contact_us">
+            <div class="card text-center">
+              <h3>First Lesson Free!</h3>
+              <div class="px-5 mb-3">
+                <p>
+                  When you make a block booking with us first lesson is included
+                  free.
+                </p>
+              </div>
+              <input type="text" placeholder="Your Name*" />
+              <input type="text" placeholder="Email*" />
+              <input type="text" placeholder="Telephone" />
+              <input type="text" placeholder="Location" />
+              <div class="request">
+                <button class="btn rounded-pill text-uppercase">
+                  Request a callback
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- /.col -->
         </div>
       </div>
     </section>
   </main>
 </template>
-
-<!-- TODO: FINIRE SQUARE E POSIZIONARLI CON POSITION ABSOLUTE -->
---
+++ src/components/main_sections/HeroSection.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "HeroSection",
+};
+</script>
+
+<template>
+  <section class="jumbotron">
+    <div class="container d-flex position-relative">
+      <h1>Drive with Avada</h1>
+      <p>We offer the finest driving tuition money can buy</p>
+      <div class="square cost rounded">
+        <div class="price">$ 39</div>
+        <div class="on_sale">On Sale</div>
+      </div>
+      <div class="square demos rounded">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-calendar3-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2H0zm0 1v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3H0z"
+          />
+        </svg>
+        <div class="on_sale">Demos</div>
+      </div>
+    </div>
+  </section>
+</template>
