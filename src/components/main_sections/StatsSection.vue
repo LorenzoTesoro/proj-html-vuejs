ec0a8aac927352f62454a13a8bd26f430169f2f3
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-23 17:16:33 +0100

    last commit

--- src/components/main_sections/StatsSection.vue
+++ src/components/main_sections/StatsSection.vue
@@ -1,10 +1,13 @@
 <script>
 import CardItem from "../utilities/CardItem.vue";
+import "vue3-circle-progress/dist/circle-progress.css";
+import CircleProgress from "vue3-circle-progress";
 
 export default {
   name: "StatsSection",
   components: {
     CardItem,
+    CircleProgress,
   },
 };
 </script>
@@ -16,8 +19,13 @@ export default {
       <div class="row">
         <div class="col">
           <CardItem>
-            <div class="circle mb-5">
-              <div class="number">95%</div>
+            <div class="circle">
+              <span class="percentage high">%</span>
+              <circleProgress
+                :percent="95"
+                :show-percent="true"
+                fill-color="#79b27b"
+              />
             </div>
             <div class="title pb-3">
               <h6 class="text-uppercase">pass rate</h6>
@@ -26,9 +34,15 @@ export default {
         </div>
         <div class="col">
           <CardItem>
-            <div class="circle mb-5">
-              <div class="number">95%</div>
+            <div class="circle">
+              <span class="percentage max">%</span>
+              <circleProgress
+                :percent="100"
+                :show-percent="true"
+                fill-color="#79b27b"
+              />
             </div>
+
             <div class="title pb-3">
               <h6 class="text-uppercase">referral rate</h6>
             </div>
@@ -36,8 +50,13 @@ export default {
         </div>
         <div class="col">
           <CardItem>
-            <div class="circle mb-5">
-              <div class="number">95%</div>
+            <div class="circle">
+              <span class="percentage min">%</span>
+              <circleProgress
+                :percent="0.2"
+                :show-percent="true"
+                fill-color="#79b27b"
+              />
             </div>
             <div class="title pb-3">
               <h6 class="text-uppercase">Accident rate</h6>
